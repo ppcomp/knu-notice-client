b8bba55406d076b8012099a0298fe65e7f1d9204
sang <rfvbnm22@naver.com>
2020-05-24 00:19:08 +0900

    style: intro 주석 추가

--- Knu_push_app/app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ Knu_push_app/app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+    import android.content.Intent
+    import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
 
-
+/**
+ * intro 기능, intro후 mainActivity 실행
+ * @author 김상은
+ */
 class SplashActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
