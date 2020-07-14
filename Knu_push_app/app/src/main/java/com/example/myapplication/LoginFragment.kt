4589e536f5722d43b68b529e8d1169bfbe6e494c
DeerGum <47655983+DeerGum@users.noreply.github.com>
2020-07-14 23:39:22 +0900

    요청 (#2) (#2)
    * feat: 로그인 화면 추가 
    
    로그인 화면 추가
    
    * refactor: Login 파일 이름 수정
    
    LoginActivity.kt
    
    * feat: Merge branch 'master' and add Firebase
    
    google API-key는 Notion 개발 전달사항에 올리겠습니다.
    
    * chore: api-key ignore
    
    * fix: git Duplicate fix
    
    * feat: 파이어베이스 Authentication 연동
    
    로그인 상태 반영해서 앱에 표시해주고 회원가입 기능 추가해야 함
    
    * feat: Kakao login api 추가
    
    * chore: Firebase api 제거 및 manifest 수정
    
    manifest에 필요없는 코드 삭제
    
    * feat: 카카오 로그인 기능 구현
    
    실제 카카오 계정으로 로그인하는 것까지 구현
    
    * chore: 사용하지 않는 파일 제거
    
    * style: 로그인 코드 정리
    
    * docs: UserInfoFragment 추가
    
    생성만 해두고 아직 사용하지는 않음

--- Knu_push_app/app/src/main/java/com/example/myapplication/LoginFragment.kt
+++ Knu_push_app/app/src/main/java/com/example/myapplication/LoginFragment.kt
@@ -1,18 +1,50 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.kakao.auth.ApiErrorCode
+import com.kakao.auth.AuthType
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.network.ErrorResult
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.LogoutResponseCallback
+import com.kakao.usermgmt.callback.MeV2ResponseCallback
+import com.kakao.usermgmt.response.MeV2Response
+import com.kakao.util.exception.KakaoException
+import com.kakao.util.helper.Utility.getPackageInfo
+import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 /**
  * 하단 바 '로그인'페이지의  kt
- * @author 희진
+ * @author 희진, 정준
  */
 class LoginFragment : Fragment() {
+    private var callback: SessionCallback = SessionCallback() //카카오에서 제공하는 콜백함수
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        callback = SessionCallback()
+        Session.getCurrentSession().addCallback(callback)
+        Session.getCurrentSession().checkAndImplicitOpen()  //로그인 이력이 있으면 재접속해도 로그인 유지해줌
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +52,91 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.activity_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_logout.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(activity, "로그아웃되었습니다.", Toast.LENGTH_SHORT).show()
+                UserManagement.getInstance().requestLogout(object : LogoutResponseCallback() {
+                    override fun onCompleteLogout() {
+                        //추후에 로그인 전, 후로 Fragment를 나누면 Fragment 전환에 사용
+                    }
+                })
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Session.getCurrentSession().removeCallback(callback)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // 카카오 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data)
+            return;
+        }
+    }
+
+    /**
+     * 카카오로그인 처리해주는 콜백함수
+     * @author 정준
+     */
+    private inner class SessionCallback : ISessionCallback {
+        override fun onSessionOpened() {
+            // 로그인 세션이 열렸을 때
+            UserManagement.getInstance().me(object : MeV2ResponseCallback() {
+                override fun onSuccess(result: MeV2Response?) {
+                    // 로그인이 성공했을 때
+                    Toast.makeText(
+                        activity,
+                        "로그인 성공! 계정 id: " + result!!.id,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onSessionClosed(errorResult: ErrorResult?) {
+                    // 로그인 도중 세션이 비정상적인 이유로 닫혔을 때
+                    Toast.makeText(
+                        activity,
+                        "세션이 닫혔습니다. 다시 시도해주세요 : ${errorResult.toString()}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onFailure(errorResult: ErrorResult?) {
+                    //로그인에 실패했을 때, 네트워크 불안정한 경우도 여기에 해당 1405916740
+                    var result = errorResult?.errorCode
+
+                    if (result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                        Toast.makeText(activity, "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT)
+                            .show()
+                        activity?.finish()
+                    } else {
+                        Toast.makeText(
+                            activity,
+                            "로그인 도중 오류가 발생했습니다: " + errorResult?.errorMessage,
+                            Toast.LENGTH_SHORT
+                        )
+                    }
+                }
+            })
+        }
+
+        override fun onSessionOpenFailed(exception: KakaoException?) {
+            // 로그인 세션이 정상적으로 열리지 않았을 때
+            if (exception != null) {
+                com.kakao.util.helper.log.Logger.e(exception)
+                Toast.makeText(
+                    activity,
+                    "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요 : $exception",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
 }
