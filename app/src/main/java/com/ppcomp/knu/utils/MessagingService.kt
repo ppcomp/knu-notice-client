2544bc6d86a5dff69086745c6b4ebc72520ee711
jeongjun Hawng <hsk2454@naver.com>
2020-08-09 02:15:05 +0900

    test: 푸쉬알림 파싱 테스트

--- app/src/main/java/com/ppcomp/knu/utils/MessagingService.kt
+++ app/src/main/java/com/ppcomp/knu/utils/MessagingService.kt
@@ -1,11 +1,14 @@
 package com.ppcomp.knu.utils
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -26,7 +29,6 @@ class MessagingService : FirebaseMessagingService() {
             Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
             sendNotification(remoteMessage.notification?.body)
         }
-
     }
     override fun onNewToken(token: String) {
         Log.d(TAG, "new Token: $token")
@@ -36,6 +38,23 @@ class MessagingService : FirebaseMessagingService() {
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             putExtra("Notification", body)
+        }
+
+        val CHANNEL_ID = "CollocNotification"
+        val CHANNEL_NAME = "CollocChannel"
+        val description = "This is Colloc channel"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+
+        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if( android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
+            channel.description = description
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.setShowBadge(false)
+            notificationManager.createNotificationChannel(channel)
         }
 
         var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
@@ -49,7 +68,6 @@ class MessagingService : FirebaseMessagingService() {
             .setSound(notificationSound)
             .setContentIntent(pendingIntent)
 
-        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0, notificationBuilder.build())
     }
 }
