34b175eb0f4c78febacae30f65a5ad4a968e053d
sang <rfvbnm22@naver.com>
2020-08-10 21:55:57 +0900

    fix : splashActicity 주석처리

--- app/src/main/java/com/ppcomp/knu/activity/SplashActivity.kt
+++ app/src/main/java/com/ppcomp/knu/activity/SplashActivity.kt
@@ -34,20 +34,19 @@ class SplashActivity : AppCompatActivity() {
 
         val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
         val ed = pref.edit()
-        val getId = pref.getString("UID", "")
-        val checkFirstUser = pref.getString("First?", "Yes")
+        val getId = pref.getString("UID", "") // UID를 불러옴(신규인지 확인하기위해)
+        val checkFirstUser = pref.getString("First?", "Yes") // 신규 사용자 구독을 위함
         var subsList = arrayListOf<Subscription>()
-        val serverUrl = "http://15.165.178.103/notice/list"
-        val subscriptionList = pref.getString("Subs", "")?.split("+")
+        val serverUrl = "http://15.165.178.103/notice/list" // Server URL
+        val subscriptionList = pref.getString("Subs", "")?.split("+") // 저장된 학과를 나눠 ArrayList에 저장 -- 체크박스를 위한 용도
         val set: MutableSet<String> = mutableSetOf("")
         if (subscriptionList != null) {
-            for (i in 0 until subscriptionList.count()) {
+            for (i in 0 until subscriptionList.count()) { // 중복 제거
                 set.add(subscriptionList[i])
             }
         }
-        // 구독리스트들을 "+" 기준으로 나눠서 set에 저장
 
-        try {
+        try { // 서버 연결하여 구독리스트 불러오고 confirmCheck 변수로 체크박스 호환
             val stream = URL(serverUrl).openConnection() as HttpURLConnection
             var read = BufferedReader(InputStreamReader(stream.inputStream, "UTF-8"))
             val response = read.readLine()
@@ -72,10 +71,9 @@ class SplashActivity : AppCompatActivity() {
         }
 
         subsList.sortWith(Comparator { data1, data2 -> data1.name.compareTo(data2.name)})
-        subsList.sortBy { data -> data.name }
+        subsList.sortBy { data -> data.name } // 정렬
 
-        val userLocalData = this.getSharedPreferences("pref", Context.MODE_PRIVATE)
-        val editor = userLocalData!!.edit()
+        val editor = pref!!.edit()                                                                                // Arraylist를 SharedPreferences에 저장
         val makeGson = GsonBuilder().create()
         var listType : TypeToken<ArrayList<Subscription>> = object : TypeToken<ArrayList<Subscription>>() {}
         var strContact = makeGson.toJson(subsList, listType.type)
@@ -87,7 +85,7 @@ class SplashActivity : AppCompatActivity() {
             val toast = Toast.makeText(this, "신규 사용자입니다. \n구독리스트 설정화면으로 이동합니다.", Toast.LENGTH_SHORT)
             toast.setGravity(Gravity.CENTER,0,0)
             toast.show()
-            var uniqueID = UUID.randomUUID().toString()
+            var uniqueID = UUID.randomUUID().toString() // 고유 ID 생성
             val userInfo = UserInfo(
                 id = uniqueID,
                 id_method = "guid",
@@ -98,7 +96,7 @@ class SplashActivity : AppCompatActivity() {
             ed.apply()
 
             val apiService = RestApiService()
-            apiService.addUser(userInfo) {
+            apiService.addUser(userInfo) { // 신규 사용자 등록
                 if (it?.id != null) {
                     // it = newly added user parsed as response
                     // it?.id = newly added user ID
@@ -107,6 +105,7 @@ class SplashActivity : AppCompatActivity() {
                 }
             }
         }
+        
         if(checkFirstUser.equals("Yes")) // 신규 사용자일시 구독리스트 설정, 아닐시 메인화면
         {
             var intent = Intent(this, SubscriptionActivity::class.java)
