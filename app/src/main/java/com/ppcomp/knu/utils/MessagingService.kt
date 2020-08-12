0c5cccc0ce3cb9193a81147539f99c35a970c5e3
jeongjun Hawng <hsk2454@naver.com>
2020-08-12 20:41:03 +0900

    fix: 푸쉬알림 코드 일부 수정

--- app/src/main/java/com/ppcomp/knu/utils/MessagingService.kt
+++ app/src/main/java/com/ppcomp/knu/utils/MessagingService.kt
@@ -1,6 +1,5 @@
 package com.ppcomp.knu.utils
 
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -8,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.media.RingtoneManager
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -27,27 +25,28 @@ class MessagingService : FirebaseMessagingService() {
 
         if(remoteMessage.notification != null) {
             Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
-            sendNotification(remoteMessage.notification?.body)
+            sendNotification(remoteMessage)
+
         }
     }
     override fun onNewToken(token: String) {
         Log.d(TAG, "new Token: $token")
     }
 
-    private fun sendNotification(body: String?) {
+    private fun sendNotification(message: RemoteMessage) {
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            putExtra("Notification", body)
+            putExtra("Notification", message.notification?.body)    // 메시지 값 전달
         }
 
-        val CHANNEL_ID = "CollocNotification"
-        val CHANNEL_NAME = "CollocChannel"
-        val description = "This is Colloc channel"
+        val CHANNEL_ID = "KeywordNotification"
+        val CHANNEL_NAME = "키워드 알림채널"
+        val description = "해당하는 키워드의 공지사항이 새로 올라오면 알려줍니다."
         val importance = NotificationManager.IMPORTANCE_HIGH
 
         var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        if( android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if( android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) { //안드로이드 오레오부터 적용되는 코드
             val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
             channel.description = description
             channel.enableLights(true)
@@ -57,15 +56,16 @@ class MessagingService : FirebaseMessagingService() {
             notificationManager.createNotificationChannel(channel)
         }
 
-        var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT) //intent를 특정 시점에 실행시킬 때 사용
         val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
         var notificationBuilder = NotificationCompat.Builder(this,"Notification")
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle("Push Notification FCM")
-            .setContentText(body)
+            .setContentText(message.notification?.body)
             .setAutoCancel(true)
             .setSound(notificationSound)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
 
         notificationManager.notify(0, notificationBuilder.build())
