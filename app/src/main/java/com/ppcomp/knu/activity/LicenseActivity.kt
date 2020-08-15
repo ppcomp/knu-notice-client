7d911630c18e09bd58e192754b1f7d9b2ea3e220
Leeheejin1 <dlgmlwls3133@naver.com>
2020-08-15 20:36:50 +0900

    style: icon 형식 svg로 변경 및 레이아웃 수정 (#37)
    * docs: 사용 안하는 icon 삭제
    
    * style: drawable png형식 -> svg로 변경
    
    몇 개 변경 안되는 icon 존재(사이트에서 사용한 icon을 못 찾았거나, 원래부터 png만 가능하도록 된 icon)
    
    * style: drawable png -> svg 변경 (추가)
    
    * style: maker, license에 toolbar 추가

--- app/src/main/java/com/ppcomp/knu/activity/LicenseActivity.kt
+++ app/src/main/java/com/ppcomp/knu/activity/LicenseActivity.kt
@@ -3,11 +3,19 @@ package com.ppcomp.knu.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ppcomp.knu.R
+import kotlinx.android.synthetic.main.activity_license_toolbar.*
+import kotlinx.android.synthetic.main.activity_subscription_toolbar.*
 
 class LicenseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_license)
+
+        setSupportActionBar(license_layout_toolbar)//toolbar 지정
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.move_back_ic)//뒤로가기 아이콘 지정
+        supportActionBar?.setDisplayShowTitleEnabled(false) //타이틀 안보이게 하기
+
     }
 }
