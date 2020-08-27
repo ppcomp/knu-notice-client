c8340f45edc5124d1bf8a38fab7634cb55f114bb
qwlake <qwlake@gmail.com>
2020-08-27 22:00:37 +0900

    fix: 실행을 위한 임시 revert

---
+++ app/src/main/java/com/ppcomp/knu/object/Notice.kt
@@ -0,0 +1,4 @@
+package com.ppcomp.knu.`object`
+
+//class Notice (val bid: String, val title: String, val date: String, val author: String)
+class Notice (val title: String, var board: String, val date: String, var author: String, var link:String, var reference:String, var fixed:Boolean, var image:Int, var fixed_image:Int, var bookmark:Boolean)
