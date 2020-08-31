08f5c263ff7c913c4b7fbbc1b6371544e8a68bb4
sang <rfvbnm22@naver.com>
2020-09-01 01:30:21 +0900

    test: 구독리스트 기능 테스트

--- app/src/main/java/com/ppcomp/knu/adapter/SubscriptionAdapter.kt
+++ app/src/main/java/com/ppcomp/knu/adapter/SubscriptionAdapter.kt
@@ -2,7 +2,6 @@ package com.ppcomp.knu.adapter
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,14 +11,20 @@ import com.ppcomp.knu.R
 import com.ppcomp.knu.`object`.Subscription
 import com.ppcomp.knu.utils.HangulUtils
 import kotlinx.android.synthetic.main.activity_subscription_item.view.*
-import org.w3c.dom.Text
 import kotlin.collections.ArrayList
 
 /**
  * item의 어느요소를 어느 View에 넣을 것인지 연결해주는 Adapter
  * @author 상은
  */
-class SubscriptionAdapter(val context: Context, var subsList: ArrayList<Subscription>, private val selectedItemCount: TextView, private val myToast: Toast) :
+class SubscriptionAdapter(
+    val context: Context,
+    var subsList: ArrayList<Subscription>,
+    private val selectedItemCount: TextView,
+    private val myToast: Toast,
+    private var checkList: ArrayList<Subscription>?,
+    private var checkListAdapter: SubscriptionCheckAdapter?
+) :
     RecyclerView.Adapter<SubscriptionAdapter.Holder>(), Filterable {
 
     var subsFilterList = ArrayList<Subscription>()
@@ -28,7 +33,7 @@ class SubscriptionAdapter(val context: Context, var subsList: ArrayList<Subscrip
         subsFilterList = subsList
     }
 
-    override fun getFilter(): Filter {
+    override fun getFilter(): Filter { // 검색 기능
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults {
                 val charSearch = constraint.toString().toLowerCase()
@@ -61,7 +66,8 @@ class SubscriptionAdapter(val context: Context, var subsList: ArrayList<Subscrip
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(context).inflate(R.layout.activity_subscription_item, parent, false)
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.activity_subscription_item, parent, false)
         selectedItemCount.text = getSelectedItemCount().toString() + "/10"
         return Holder(view)
     }
@@ -86,21 +92,27 @@ class SubscriptionAdapter(val context: Context, var subsList: ArrayList<Subscrip
                 _, isChecked ->
             val getCheckedCount = getSelectedItemCount()
             val getName: String = holder.itemView.subs_name.text as String
-            if(getCheckedCount >= 10 && isChecked){ // 체크가 10개를 초과한 경우에서 체크를 할 때
-                    myToast.setText("구독리스트가 10개를 초과했습니다")
-                    myToast.show()
-                    holder.chk?.isChecked = false
-            } else{
+            if (getCheckedCount >= 10 && isChecked) { // 체크가 10개를 초과한 경우에서 체크를 할 때
+                myToast.setText("구독리스트가 10개를 초과했습니다")
+                myToast.show()
+                holder.chk?.isChecked = false
+            } else {
                 for (i in subsList) {
                     if (i.name == getName) {
                         i.checked = isChecked
+                            if (isChecked && checkList?.contains(i) == false) {
+                                checkList?.add(i) // 체크한경우 체크리스트에 체크값 추가
+                            } else if(!isChecked) {
+                                checkList?.remove(i) // 체크해제한 경우 값 제거
+                            }
+                        checkListAdapter?.notifyDataSetChanged()
+                        }
                     }
                 }
                 selectedItemCount.text = getSelectedItemCount().toString() + "/10"
             }
 
-        }
-        holder.name?.setOnClickListener{ // 학과 이름 누르면 체크되도록
+        holder.name?.setOnClickListener { // 학과 이름 누르면 체크되도록
             holder.chk?.isChecked = !holder.chk?.isChecked!!
         }
 
@@ -129,8 +141,8 @@ class SubscriptionAdapter(val context: Context, var subsList: ArrayList<Subscrip
     }
 
     fun setCheckAll(boolean: Boolean) {
-        for(ckbox in subsList){
-            if(ckbox.checked == !boolean)
+        for (ckbox in subsList) {
+            if (ckbox.checked == !boolean)
                 ckbox.checked = boolean
 
         }
@@ -138,12 +150,24 @@ class SubscriptionAdapter(val context: Context, var subsList: ArrayList<Subscrip
     }
 
     private fun getSelectedItemCount(): Int {
-        var count = 0
-        for(ckbox in subsList){
-            if(ckbox.checked)
-               count++
+        return if (checkList != null) {
+            var count = 0
+            for (ckbox in subsList) {
+                if (ckbox.checked)
+                    count++
+            }
+            count
+        }else{
+            0
         }
-        return count
+    }
+
+    fun setCheckList(getCheckList : ArrayList<Subscription>){
+        checkList = getCheckList
+    }
+
+    fun setCheckListAdapter(getCheckListAdapter : SubscriptionCheckAdapter){
+        checkListAdapter = getCheckListAdapter
     }
 
     override fun getItemId(position: Int): Long {
---
+++ app/src/main/java/com/ppcomp/knu/adapter/SubscriptionCheckAdapter.kt
@@ -0,0 +1,107 @@
+package com.ppcomp.knu.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import com.ppcomp.knu.R
+import com.ppcomp.knu.`object`.Subscription
+import com.ppcomp.knu.utils.HangulUtils
+import kotlinx.android.synthetic.main.activity_subscription_item.view.*
+import kotlin.collections.ArrayList
+
+/**
+ * item의 어느요소를 어느 View에 넣을 것인지 연결해주는 Adapter
+ * @author 상은
+ */
+class SubscriptionCheckAdapter(
+    val context: Context,
+    var subsCheckList: ArrayList<Subscription>,
+    private val selectedItemCount: TextView,
+    private val subsList: ArrayList<Subscription>,
+    private val subsAdapter: SubscriptionAdapter
+) :
+    RecyclerView.Adapter<SubscriptionCheckAdapter.Holder>() {
+
+    var checkedList = ArrayList<Subscription>()
+
+    init {
+        checkedList = subsCheckList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.activity_subscription_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return checkedList.size
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+
+        holder.bind(checkedList[position], context)
+
+        holder.chk?.setOnCheckedChangeListener(null)
+        // 체크박스 부분
+
+        holder.chk?.isChecked = checkedList[position].checked
+
+        holder.chk?.setOnCheckedChangeListener { // 체크 표시할 때
+                _, isChecked ->
+            if (!isChecked) {
+                val getName: String = holder.itemView.subs_name.text as String
+                for (i in subsCheckList) { // 체크한 아이템의 이름으로 위치찾기
+                    if (i.name == getName) {
+                        subsCheckList.remove(i)
+                        for (j in subsList) { // 전체 구독리스트를 나타내는 리사이클러뷰의 체크한 아이템 체크 해제
+                            if (j.name == getName) {
+                                j.checked = false
+                                subsAdapter.notifyDataSetChanged()
+                            }
+                        }
+                        notifyDataSetChanged()
+                        break
+                    }
+                }
+                selectedItemCount.text = checkedList.size.toString() + "/10"
+            }
+
+        }
+
+        holder.name?.setOnClickListener { // 학과 이름 누르면 체크되도록
+            holder.chk?.isChecked = !holder.chk?.isChecked!!
+        }
+
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val name = itemView?.findViewById<TextView>(R.id.subs_name)
+        val chk = itemView?.findViewById<CheckBox>(R.id.subs_checkbox)
+
+        fun bind(subscription: Subscription, context: Context) {
+            name?.text = subscription.name
+            chk?.isChecked = subscription.checked
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+
+}
+
+
+
