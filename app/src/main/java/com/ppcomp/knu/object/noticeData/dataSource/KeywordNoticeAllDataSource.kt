52d01e7c2c984d10330430116acd020323163029
DeerGum <47655983+DeerGum@users.noreply.github.com>
2020-09-17 22:27:12 +0900

    fix: 서버 연결 안되면 튕기는 현상 수정 (#86)

--- app/src/main/java/com/ppcomp/knu/object/noticeData/dataSource/KeywordNoticeAllDataSource.kt
+++ app/src/main/java/com/ppcomp/knu/object/noticeData/dataSource/KeywordNoticeAllDataSource.kt
@@ -15,21 +15,19 @@ class KeywordNoticeAllDataSource(private val restApi: RestApi,
     @SuppressLint("CheckResult")
     override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Notice>) {
         val curr = 1
-        try {
-            restApi.getNoticeAll(q=q, target=target, page=curr)
-                .subscribe { it ->
-                    if (it.next == null)
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList,q), null, null)
-                        }
-                    else
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList,q), null, curr + 1)
-                        }
-                }
-        } catch (e: Exception) {
-            GlobalApplication.isServerConnect = false
-        }
+        restApi.getNoticeAll(q=q, target=target, page=curr)
+            .subscribe( { it ->
+                if (it.next == null)
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList,q), null, null)
+                    }
+                else
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList,q), null, curr + 1)
+                    }
+            },{
+                GlobalApplication.isServerConnect = false
+            })
     }
 
     override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Notice>) {
@@ -38,18 +36,16 @@ class KeywordNoticeAllDataSource(private val restApi: RestApi,
 
     @SuppressLint("CheckResult")
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Notice>) {
-        try {
-            restApi.getNoticeAll(q=q, target=target, page=params.key)
-                .subscribe { it ->
-                    if (it.next == null)
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList,q),null) }
-                    else
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList,q),params.key+1) }
-                }
-        } catch (e: Exception) {
-            GlobalApplication.isServerConnect = false
-        }
+        restApi.getNoticeAll(q=q, target=target, page=params.key)
+            .subscribe( { it ->
+                if (it.next == null)
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList,q),null) }
+                else
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList,q),params.key+1) }
+            }, {
+                GlobalApplication.isServerConnect = false
+            })
     }
 }
