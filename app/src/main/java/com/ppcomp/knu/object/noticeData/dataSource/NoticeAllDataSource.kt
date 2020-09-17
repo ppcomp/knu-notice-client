d0e03bc842ff04e10e45fc54ca0f4b3677cc927c
jeongjun Hawng <hsk2454@naver.com>
2020-09-17 22:26:12 +0900

    fix: 서버 연결 안되면 튕기는 현상 수정

--- app/src/main/java/com/ppcomp/knu/object/noticeData/dataSource/NoticeAllDataSource.kt
+++ app/src/main/java/com/ppcomp/knu/object/noticeData/dataSource/NoticeAllDataSource.kt
@@ -22,21 +22,19 @@ class NoticeAllDataSource(
     @SuppressLint("CheckResult")
     override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Notice>) {
         val curr = 1
-        try {
-            restApi.getNoticeAll(q=q, target=target!!, page=curr)
-                .subscribe { it ->
-                    if (it.next == null)
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList), null, null)
-                        }
-                    else
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList), null, curr + 1)
-                        }
-                }
-        } catch (e: Exception) {
-            GlobalApplication.isServerConnect = false
-        }
+        restApi.getNoticeAll(q=q, target=target!!, page=curr)
+            .subscribe ({ it ->
+                if (it.next == null)
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList), null, null)
+                    }
+                else
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList), null, curr + 1)
+                    }
+            }, {
+                GlobalApplication.isServerConnect = false
+            })
     }
 
     override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Notice>) {
@@ -45,18 +43,16 @@ class NoticeAllDataSource(
 
     @SuppressLint("CheckResult")
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Notice>) {
-        try {
-            restApi.getNoticeAll(q=q, target=target!!, page=params.key)
-                .subscribe { it ->
-                    if (it.next == null)
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList),null) }
-                    else
-                        it.results?.let { noticeList ->
-                            callback.onResult(injectDataToNotices(noticeList),params.key+1) }
-                }
-        } catch (e: Exception) {
-            GlobalApplication.isServerConnect = false
-        }
+        restApi.getNoticeAll(q=q, target=target!!, page=params.key)
+            .subscribe ({ it ->
+                if (it.next == null)
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList),null) }
+                else
+                    it.results?.let { noticeList ->
+                        callback.onResult(injectDataToNotices(noticeList),params.key+1) }
+            },{
+                GlobalApplication.isServerConnect = false
+            })
     }
 }
