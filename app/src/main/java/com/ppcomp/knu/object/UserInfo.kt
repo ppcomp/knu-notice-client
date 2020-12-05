cdb1570d85886ae39adb34594428f07d86e6bf76
DeerGum <47655983+DeerGum@users.noreply.github.com>
2020-12-05 20:37:51 +0900

    fix: UserInfo Class device_id 명칭 수정 (#99)
    device_id -> device

--- app/src/main/java/com/ppcomp/knu/object/UserInfo.kt
+++ app/src/main/java/com/ppcomp/knu/object/UserInfo.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class UserInfo (
     @SerializedName("id") val id: String?,
 //    @SerializedName("email") val email: String?,
-    @SerializedName("device_id") val device_id: String?
+    @SerializedName("device") val device: String?
 )
